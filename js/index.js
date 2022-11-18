160d26c5d90ccdbeb230b0026f54ff2e4c2e79b2
ThomasLaumonier <thomaslaumonier@orange.fr>
2022-11-18 15:25:06 +0200

    v0.9.3

--- js/index.js
+++ js/index.js
@@ -19,11 +19,12 @@ tl.to(".services__main__left__1",{translateX: "100%"}).to(".services__main__righ
 ScrollTrigger.create({
     animation: tl,
     trigger: ".services",
-    start: "-20px top",
+    start: "-20px top", 
     end:"+=1400px",  
     scrub: true,
     pin: true, 
     toogleActions: "restart pause reverse pause",
+    anticipatePin: 2,
 });
 //anticipatePin: 1 si ça continue de scroll 
 
@@ -31,126 +32,6 @@ function test()
 {
     console.log("test"); 
 }
-
-function ecosystemLeft2to1()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "Digital Art";
-    digitalArt.style.left = "0%";
-    utility.style.left = "0%";
-    gamification.style.left = "0%";
-    dao.style.left = "0%";
-    leftTriangle.setAttribute("onclick", "");
-    rightTriangle.setAttribute("onclick", "ecosystemRight1to2()");
-    leftTriangle.style.cursor = "not-allowed"; 
-}
-
-function ecosystemLeft3to2()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "Utility";
-    digitalArt.style.left = "-100%";
-    utility.style.left = "-100%";
-    gamification.style.left = "-100%";
-    dao.style.left = "-100%";
-    leftTriangle.setAttribute("onclick", "ecosystemLeft2to1()");
-    rightTriangle.setAttribute("onclick", "ecosystemRight2to3()");
-
-}
-
-function ecosystemLeft4to3()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "Gamification";
-    digitalArt.style.left = "-200%";
-    utility.style.left = "-200%";
-    gamification.style.left = "-200%";
-    dao.style.left = "-200%";
-    leftTriangle.setAttribute("onclick", "ecosystemLeft3to2()");
-    rightTriangle.setAttribute("onclick", "ecosystemRight3to4()");
-    rightTriangle.style.cursor = "pointer"; 
-}
-
-function ecosystemRight1to2()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "Utility";
-    digitalArt.style.left = "-100%";
-    utility.style.left = "-100%";
-    gamification.style.left = "-100%";
-    dao.style.left = "-100%";
-    leftTriangle.setAttribute("onclick", "ecosystemLeft2to1()");
-    rightTriangle.setAttribute("onclick", "ecosystemRight2to3()");
-    leftTriangle.style.cursor = "pointer";
-}
-
-function ecosystemRight2to3()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "Gamification";
-    digitalArt.style.left = "-200%";
-    utility.style.left = "-200%";
-    gamification.style.left = "-200%";
-    dao.style.left = "-200%";
-    leftTriangle.setAttribute("onclick", "ecosystemLeft3to2()");
-    rightTriangle.setAttribute("onclick", "ecosystemRight3to4()");
-}
-
-function ecosystemRight3to4()
-{
-    const titre = document.getElementById("ecosystem_titre");
-    const digitalArt = document.getElementById("digitalArt");
-    const utility = document.getElementById("utility");
-    const gamification = document.getElementById("gamification");
-    const dao = document.getElementById("dao");
-    const leftTriangle = document.getElementById("ecosystem_triangle_left");
-    const rightTriangle = document.getElementById("ecosystem_triangle_right");
-
-    titre.textContent = "DAO";
-    digitalArt.style.left = "-300%";
-    utility.style.left = "-300%";
-    gamification.style.left = "-300%";
-    dao.style.left = "-300%";
-    leftTriangle.setAttribute("onclick", "ecosystemLeft4to3()");
-    rightTriangle.setAttribute("onclick", "");
-    rightTriangle.style.cursor = "not-allowed";
-}
-
 
 
 // gsap.to(".services__main__left__1",{
